9ced92e284ff8cbbfed189d9f09e4f09d488c897
asimakcay <asimakcay97@gmail.com>
2024-08-16 09:25:38 +0300

    Update ui.R

--- OEE-Calculation/ui.R
+++ OEE-Calculation/ui.R
@@ -1,8 +1,10 @@
 library(shiny)
 library(plotly)
 
+turkce_aylar <- c("Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", 
+                  "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık")
+
 ui <- fluidPage(
-  
   titlePanel("OEE Hesaplama Aracı"),
   
   sidebarLayout(
@@ -17,20 +19,26 @@ ui <- fluidPage(
       numericInput("IdealRate", "İdeal Üretim Hızı (parça/sa):", value = 0),
       numericInput("TotalPieces", "Toplam Üretilen Parça:", value = 0),
       numericInput("RejectedPieces", "Reddedilen Parça:", value = 0),
+      selectInput("month", "Ay Seçin:", choices = turkce_aylar),
       
-      actionButton("calculate", "Hesapla")
+      actionButton("calculate", "Hesapla"),
+      downloadButton("downloadData", "Verileri Excel Olarak İndir")
     ),
     
     mainPanel(
       fluidRow(
         column(4,
                h3("Sonuçlar"),
+               textOutput("plannedProduction"),
+               textOutput("operatingTime"),
+               textOutput("goodPieces"),
                textOutput("availability"),
                textOutput("performance"),
                textOutput("quality")
         ),
         column(8,
-               plotlyOutput("oeeGauge", height = "400px", width = "100%")
+               plotlyOutput("oeeGauge", height = "400px", width = "100%"),
+               plotlyOutput("monthlyOEEPlot", height = "400px", width = "100%") 
         )
       )
     )
